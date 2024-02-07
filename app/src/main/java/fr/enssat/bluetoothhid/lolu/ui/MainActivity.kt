4635dece5ea8ae739b76af524729cb769280adb6
Louis Legrand <l.legrand@openium.fr>
2024-02-07 08:41:15 +0100

    Ajout de l'icon

--- app/src/main/java/fr/enssat/bluetoothhid/lolu/ui/MainActivity.kt
+++ app/src/main/java/fr/enssat/bluetoothhid/lolu/ui/MainActivity.kt
@@ -1,18 +1,8 @@
 package fr.enssat.bluetoothhid.lolu.ui
 
-import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import fr.enssat.bluetoothhid.lolu.ui.main.MainScreen
 import fr.enssat.bluetoothhid.lolu.ui.theme.LoLuTheme
